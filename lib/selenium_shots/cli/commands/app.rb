e175724cb37332219b30f9a931a07c166d0dbe88
mauro torres <maurotorres@gmail.com>
2010-04-14 10:24:23 -0300

    added messages output

--- lib/selenium_shots/cli/commands/app.rb
+++ lib/selenium_shots/cli/commands/app.rb
@@ -4,7 +4,9 @@ module SeleniumShots::Command
 		  name    = args.shift.downcase.strip rescue nil
       if name
         api_key ||= SeleniumShots::Command.run_internal('auth:api_key', args)
-		    display "Created #{name}" if make_config_file(name, api_key) == "y"
+        if make_config_file(name, api_key) == "y"
+		      display "Created #{name}\nYou can configurate selenium shots on config/selenium_shots.yml"
+        end
       else
         display "You need specify a name for your app. Run 'selenium_shots help' for usage information"
       end
