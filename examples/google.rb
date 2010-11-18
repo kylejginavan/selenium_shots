39c86042b1855f4551b9056ad21189a403c3ef6e
Kyle J. Ginavan <kylejginavan@gmail.com>
2010-11-18 13:07:31 -0600

    er

--- examples/google.rb
+++ examples/google.rb
@@ -7,9 +7,9 @@ class Google < SeleniumShots
   selenium_shot "should search on google" do
     @name = "selenium shots"
     browser.open "/"
+    browser.wait_for_page_to_load "30000"
     browser.type "q", "Selenium Shots"
     browser.click "btnG"
   end
-
 end
 
